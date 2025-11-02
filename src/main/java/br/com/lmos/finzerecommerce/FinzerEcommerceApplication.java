5b0832dfbd9241af76bb094326c12335b75862bd
lmos0 <luismarcio.os@outlook.com>
2025-11-02 17:07:34 -0300

    first commit (part 2)

---
+++ src/main/java/br/com/lmos/finzerecommerce/FinzerEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lmos.finzerecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzerEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinzerEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lmos/finzerecommerce/FinzerEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lmos.finzerecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinzerEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
